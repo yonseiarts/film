547e254b1ad6cd7846e510197b74de3f8a80c010
yonseiarts <ehrha44@gmail.com>
2025-03-23 01:13:06 +0900

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,9 +1,21 @@
-// 실제 Firebase 프로젝트 설정으로 교체해 주세요
-export const firebaseConfig = {
-  apiKey: "AIzaSyXXXXXX",
-  authDomain: "yourproject.firebaseapp.com",
-  projectId: "yourproject",
-  storageBucket: "yourproject.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdefg12345"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAhiHGN4Te72lL0SfJIIEbre7eBYcfzJ5w",
+  authDomain: "yonsei-rental.firebaseapp.com",
+  projectId: "yonsei-rental",
+  storageBucket: "yonsei-rental.firebasestorage.app",
+  messagingSenderId: "701854349149",
+  appId: "1:701854349149:web:18fa86ab435b0991769ab4",
+  measurementId: "G-M604C04PLS"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
