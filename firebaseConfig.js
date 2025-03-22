e45cf217fad693810ac1bb7293e9d47de4ff3aa7
yonseiarts <ehrha44@gmail.com>
2025-03-23 00:08:11 +0900

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// 실제 Firebase 프로젝트 설정으로 교체해 주세요
+export const firebaseConfig = {
+  apiKey: "AIzaSyXXXXXX",
+  authDomain: "yourproject.firebaseapp.com",
+  projectId: "yourproject",
+  storageBucket: "yourproject.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdefg12345"
+};
